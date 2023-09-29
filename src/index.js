d2ae3c5c59947692142dbfd9773aa70657c53a5c
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-09-29 18:42:31 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const port = 5000;
+const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
 const {Server} = require('ws');
+const port = 5000;
+
 
 app.use(cors());
 const server = http.createServer(app)
 
-const ws_server = new Server({ noServer: true });
+const ws_server = new Server({ 
+  noServer: true,
+});
 
 ws_server.on('connection',(socket)=>{
-console.log(`Client connected: ${socket.id}`);
+  const userId = uuidv4()
+console.log(`Client connected: ${userId}`);
 
 socket.on('message',(message)=>{
  console.log(`Received message: ${message}`);
@@ -19,7 +24,7 @@ socket.on('message',(message)=>{
 });
 
 socket.on('disconnect',()=>{
-    console.log('User Disconnected', socket.id);
+    console.log(`Client disconnected: ${userId}`);
 })
 });
 
@@ -28,8 +33,6 @@ server.on('upgrade', (request, socket, head) => {
       ws_server.emit('connection', socket, request);
     });
   });
-
-
 
 server.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
